c92f115fed4721d09a7072bf009378ab8be1cb64
Erivânio Vasconcelos <erivanio@MacBook-Air-de-Erivanio.local>
2022-04-04 16:51:29 -0300

    adicionando estilo do formulario de busca no componente

--- src/components/form-busca.component.ts
+++ src/components/form-busca.component.ts
@@ -1,11 +1,8 @@
-import { html, LitElement } from 'lit';
+import { html, LitElement, TemplateResult } from 'lit';
 import { customElement, property } from 'lit/decorators.js';
-import { dashboardStyles } from '../assets/css/components/dashboard.css';
 
 @customElement('form-busca')
 export class FormBusca extends LitElement {
-  static styles = dashboardStyles;
-
   @property({ type: String })
   sigla = 'MPV';
   @property({ type: Number })
@@ -13,43 +10,84 @@ export class FormBusca extends LitElement {
   @property({ type: Number })
   ano = 2020;
 
-  render() {
-    return html`
-      <div class="container">
-        <form class="was-validated eta-search-form mt-3 mt-md-5 mb-3 mb-md-5">
-          <input
-            type="search"
-            class="form-control eta-search-input"
-            placeholder="Sigla"
-            aria-label="Sigla"
-            .value=${this.sigla}
-            required="required"
-          />
-          <input
-            type="number"
-            class="form-control eta-search-input"
-            placeholder="Número"
-            aria-label="Número"
-            .valueAsNumber=${this.numero}
-          />
-          <input
-            type="number"
-            class="form-control eta-search-input"
-            placeholder="Ano"
-            aria-label="Ano"
-            required="required"
-            .valueAsNumber=${this.ano}
-          />
-          <button
-            type="button"
-            :disabled="isDisabled"
-            @click="emitirEventoCardClick"
-            class="btn btn-primary eta-search-button"
-          >
-            Pesquisar
-          </button>
-        </form>
-      </div>
-    `;
+  createRenderRoot(): LitElement {
+    return this;
+  }
+
+  // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+  render(): TemplateResult {
+    {
+      return html`
+        <style>
+          .bs-eta {
+            overflow: hidden;
+          }
+          .eta-search-form {
+            display: flex;
+            flex-direction: row;
+            align-items: center;
+            padding: 0px;
+            background: #ffffff;
+            border: 1px solid #d2d2d2;
+            box-shadow: 0px -10px 20px rgba(255, 255, 255, 0.7),
+              0px 10px 20px rgba(0, 0, 0, 0.05);
+            border-radius: 20px;
+            max-width: var(--eta-sm);
+            margin: 0 auto;
+          }
+
+          .bs-eta .eta-search-input {
+            border-radius: 0;
+            border: 0;
+            margin-right: 4px;
+            padding: 0.375rem 0 0.375rem 16px;
+          }
+
+          .bs-eta .eta-search-input:nth-child(1) {
+            border-radius: 20px 0px 0px 20px;
+          }
+
+          .bs-eta .eta-search-button {
+            border-radius: 0px 20px 20px 0px;
+            background-color: var(--etl-green);
+          }
+        </style>
+        <div class="container">
+          <form class="was-validated eta-search-form mt-3 mt-md-5 mb-3 mb-md-5">
+            <input
+              type="search"
+              class="form-control eta-search-input"
+              placeholder="Sigla"
+              aria-label="Sigla"
+              .value=${this.sigla}
+              required="required"
+            />
+            <input
+              type="number"
+              class="form-control eta-search-input"
+              placeholder="Número"
+              aria-label="Número"
+              .valueAsNumber=${this.numero}
+            />
+            <input
+              type="number"
+              class="form-control eta-search-input"
+              placeholder="Ano"
+              aria-label="Ano"
+              required="required"
+              .valueAsNumber=${this.ano}
+            />
+            <button
+              type="button"
+              :disabled="isDisabled"
+              @click="buscar"
+              class="btn btn-primary eta-search-button"
+            >
+              Pesquisar
+            </button>
+          </form>
+        </div>
+      `;
+    }
   }
 }

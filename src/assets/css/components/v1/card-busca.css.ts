d6be2efd860612cced96fe63b13be752674464e3
Erivânio Vasconcelos <erivanio@MacBook-Air-de-Erivanio.local>
2022-04-20 11:54:19 -0300

    cria componente card-busca

---
+++ src/assets/css/components/v1/card-busca.css.ts
@@ -0,0 +1,21 @@
+import { css } from 'lit';
+
+export const cardBuscaCSS = css`
+  .card-header {
+    max-width: 300px;
+  }
+
+  .card-header [slot='header'] {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
+
+  .card-header h3 {
+    margin: 0;
+  }
+
+  .card-header sl-icon-button {
+    font-size: var(--sl-font-size-medium);
+  }
+`;

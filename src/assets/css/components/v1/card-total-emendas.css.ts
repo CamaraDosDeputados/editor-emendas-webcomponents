807408cc60d4d8c7cc89db4069e407096929f858
Erivânio Vasconcelos <erivanio@MacBook-Air-de-Erivanio.local>
2022-04-20 11:57:08 -0300

    cria componente card-total-emendas

---
+++ src/assets/css/components/v1/card-total-emendas.css.ts
@@ -0,0 +1,12 @@
+import { css } from 'lit';
+
+export const cardTotalEmendasCSS = css`
+  .card-dashboard {
+    max-width: 300px;
+  }
+
+  .card-dashboard sl-icon {
+    font-size: var(--sl-font-size-large);
+    float: right;
+  }
+`;
